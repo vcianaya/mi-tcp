612a403d050ea55f80f29c860223c7db152ceb8e
unknown <vcianya>
2018-01-07 09:22:21 -0400

    tcp-server

--- tools/device.js
+++ tools/device.js
@@ -3,7 +3,18 @@ EventEmitter = require('events').EventEmitter;
 util.inherits(Device, EventEmitter);
 function Device(adapter, connection, gpsServer) {
     EventEmitter.call(this);
-    console.log('llegue aqui');
+    var _this = this;
+
+    this.connection = connection;
+    this.server = gpsServer;
+    this.adapter = adapter.adapter(this);
+
+    this.uid = false;
+    this.ip = connection.ip;
+    this.port = connection.port;
+    this.name = false;
+    this.loged = false;
+    console.log(this.connection);
 }
 
 module.exports = Device;
