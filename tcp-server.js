4b118dbdd2698f1cbe149162b99045de4fa64a4e
unknown <vcianya>
2018-01-07 09:24:34 -0400

    tcp-server

--- tcp-server.js
+++ tcp-server.js
@@ -3,7 +3,7 @@ Device = require('../mi-tcp/tools/device');
 var _this = this;
 var contador = 0;
 _this.server = net.createServer(function (connection) {
-    connection.device = new Device('GT06', connection, _this);   
+    connection.device = new Device(GT06, connection, _this);   
     connection.on('data', function (data) {
         contador = contador+1;
         console.log("TCP SERVER UP  "+contador);
